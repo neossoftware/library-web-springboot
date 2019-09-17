2297ade6ff9450972cbe3c234015c464f3d4c379
hhugohm <hhugohm@gmail.com>
2019-09-17 17:25:18 -0500

    add scripts DB for AUTHOR

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,5 @@
 INSERT INTO customer(name,lastname,address) VALUES ('HUGO','HIDALGO','AV BENITO JUAREZ');
 INSERT INTO customer(name,lastname,address) VALUES ('VERONICA','COLIN','SAUCOS MEX');
+
+INSERT INTO author(name,lastname) VALUES ('JHON','KINELLY');
+INSERT INTO author(name,lastname) VALUES ('RALFP','DUGHETY');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,18 @@
 DROP TABLE IF EXISTS customer;
-
+DROP TABLE IF EXISTS author;
 CREATE TABLE customer
 (
     id BIGINT NOT NULL AUTO_INCREMENT,
     name VARCHAR(255),
     lastname VARCHAR(255),
     address VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE author
+(
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255),
+    lastname VARCHAR(255),
     PRIMARY KEY (id)
 );
